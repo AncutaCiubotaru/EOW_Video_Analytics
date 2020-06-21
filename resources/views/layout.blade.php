364072e3358d237fe1154c48beac2101c54d146f
Anca Ciubotaru <ancuta.ciubotaru@gmail.com>
2020-06-21 21:50:24 +0200

    videos work independent js works

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>EOW Videos</title>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+        @yield('content')
+
+@yield('scripts')
+
+
+</html>
--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-    <title>EOW Videos</title>
-
-    <script
-            src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-</head>
-
-<div class="container-fluid">
-    <div class="row mt-3 ml-5">
-        <ul class="list-group">
-            <li class="list-group-item"><a href="/videos/182932029">Video 1</a></li>
-            <li class="list-group-item"><a href="/videos/112836958">Video 2</a></li>
-            <li class="list-group-item"><a href="/videos/284834103">Video 3</a></li>
-            <li class="list-group-item"><a href="/videos/3689581">Video 4</a></li>
-            <li class="list-group-item"><a href="/videos/2238631">Video 5</a></li>
-        </ul>
+@section('content')
+    <div class="container-fluid">
+        <div class="row mt-3 ml-5">
+            <ul class="list-group">
+                <li class="list-group-item"><a href="/videos/182932029">Video 1</a></li>
+                <li class="list-group-item"><a href="/videos/112836958">Video 2</a></li>
+                <li class="list-group-item"><a href="/videos/284834103">Video 3</a></li>
+                <li class="list-group-item"><a href="/videos/3689581">Video 4</a></li>
+                <li class="list-group-item"><a href="/videos/2238631">Video 5</a></li>
+            </ul>
+        </div>
     </div>
-</div>
-
-
-</html>
+@endsection
